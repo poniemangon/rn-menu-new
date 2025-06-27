04c2262d97617d668934071e2de1a5f7c6ff34a3
gon_14 <poniemangon@gmail.com>
2025-06-27 13:20:57 -0300

    alpha

--- scripts.js
+++ scripts.js
@@ -1,13 +1,9 @@
 $(document).ready(function () {
     function toggleLogos() {
-      if ($(window).scrollTop() === 0) {
-        $('#logo-default').addClass('hidden');
-        $('#logo-negative').removeClass('hidden');
+      if ($(window).scrollTop() === 0 && !($('.burger').hasClass('opened'))) {
         $('.header-section').removeClass('scrolled');
       } else {
         $('.header-section').addClass('scrolled');
-        $('#logo-default').removeClass('hidden');
-        $('#logo-negative').addClass('hidden');
       }
     }
     toggleLogos();
@@ -68,6 +64,44 @@ $('.categories li').on('click', function () {
 
     $('.links-col ul.links').removeClass('active');
     $('.links-col ul.links[data-category="' + category + '"]').addClass('active');
+});
+
+$('.burger').on('click', function(){
+    $('.burger').toggleClass('opened');
+    $('.nav-col').toggleClass('opened');
+    $('.header-section').toggleClass('scrolled');
+    $('.header-section').toggleClass('opened');
+    
+    // Toggle body scrolling prevention
+    if ($('.header-section').hasClass('opened')) {
+        $('body').addClass('header-opened');
+    } else {
+        $('body').removeClass('header-opened');
+    }
+});
+
+// Mobile Navigation Functionality
+$('.mobile-nav-item-header').on('click', function() {
+    const $navItem = $(this).closest('.mobile-nav-item');
+    const $otherNavItems = $('.mobile-nav-item').not($navItem);
+    
+    // Close other nav items
+    $otherNavItems.removeClass('opened');
+    
+    // Toggle current nav item
+    $navItem.toggleClass('opened');
+});
+
+$('.mobile-category-header').on('click', function(e) {
+    e.stopPropagation(); // Prevent nav item from closing
+    const $category = $(this).closest('.mobile-category');
+    const $otherCategories = $(this).closest('.mobile-nav-categories').find('.mobile-category').not($category);
+    
+    // Close other categories in the same nav item
+    $otherCategories.removeClass('opened');
+    
+    // Toggle current category
+    $category.toggleClass('opened');
 });
 
 
